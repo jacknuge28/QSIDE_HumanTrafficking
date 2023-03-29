4062da0b0ebf80c46265773825b5c0c562a1e81a
Jack Nugent <nugentj3@msu.edu>
2023-03-29 15:17:26 -0400

    Add error handling for Extract.py

--- Extract.py
+++ Extract.py
@@ -4,27 +4,33 @@
 import tabula
 import pandas as pd
 
-def extract(url, pages, prefix):
+def extract(url, prefix, pages='all'):
     '''
     Extracts tables from a PDF at a given link and stores them as CSVs in the Data Folder.
-    Example call: extract("<URL>", 12, "001")
+    Example call: extract("<URL>", "001", 12)
     
     Parameters:
         - url (str): URL to extract tables from.
         
+        - prefix (str): Tagging number used in Data Folder and Data Bibliography. 
+            Examples: '001', '013'
+            
         - pages (str, int, iterable of int, optional): An optional values specifying pages to extract from. 
           It allows str,`int`, iterable of :int. Default: 1
             Examples: '1-2, 3', 'all', [1,2]
-        
-        - prefix (str): Tagging number used in Data Folder and Data Bibliography. 
-            Examples: '001', '013'
     '''
     print("Scanning URL...")
-    table = tabula.read_pdf(url, pages=pages)
+    try:
+        table = tabula.read_pdf(url, pages=pages, silent=True)
+        print("Scan Complete.")
+    except Exception as e:
+        table = []
+        print("Error: No Tables Found.")
     
-    for i in range(len(table)):
-        print("Saving Tables to Data/Raw_Data...\t{}/{}".format(i+1, len(table)))
-        name_str = prefix + '.' + str(i+1)
-        table[i].to_csv(f'./Data/Raw_Data/{name_str}.csv')
-        
-    print("Saving Tables Complete.")
+    if len(table) > 0:
+        for i in range(len(table)):
+            print("Saving Tables to Data/Raw_Data...\t{}/{}".format(i+1, len(table)))
+            name_str = prefix + '.' + str(i+1)
+            table[i].to_csv(f'./Data/Raw_Data/{name_str}.csv')
+
+        print("Saving Tables Complete.")
